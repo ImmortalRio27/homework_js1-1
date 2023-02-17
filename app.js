d751d50673f53c25861da4b67fbee342c759cff6
Immortal <109055473+ImmortalRio27@users.noreply.github.com>
2023-02-17 14:46:27 +0500

    Delete app.js
    Signed-off-by: Immortal <109055473+ImmortalRio27@users.noreply.github.com>

--- app.js
+++
@@ -1,5 +0,0 @@
-let x = parseInt(prompt('Введите 1 число: '));
-let y = parseInt(prompt('Введите 2 число: '));
-let z = parseInt(prompt('Введите 3 число: '));
-let res = (x + y + z) / 3;
-alert(res);
